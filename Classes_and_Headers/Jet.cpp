a8411b48ca39d93f50c318e640902ef27c417d54
devEricA <eric.anderson413@gmail.com>
2022-02-23 17:11:15 -0600

    WIP: Jet Implememntations

---
+++ Classes_and_Headers/Jet.cpp
@@ -0,0 +1,12 @@
+/*
+    This file defines the methods and constructors of the Jet class
+*/
+
+#include <iostream>
+#include "Jet.h"
+#include "Aircraft.cpp"
+using namespace std;
+
+Jet::Jet(){
+
+} 
---
+++ Unit Tests/JetTest.cpp
@@ -0,0 +1,14 @@
+/*
+    This file is responsible for testing the Jet class
+    in order to ensure that all code is covered.
+    This file is compiled seperately from the main program. 
+*/
+
+#include <iostream>
+#include "../Classes_and_Headers/Jet.cpp"
+using namespace std;
+
+int main(){
+
+    return 0;
+}
